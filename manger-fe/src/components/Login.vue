3dcd5e6748c0f8f0514ed14b894cd10d59a9ff9e
Gonwe <gonwe@qq.com>
2021-07-14 23:16:15 +0800

    feat:路由封装

---
+++ manger-fe/src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>登录页面{{ msg }}</h1>
+  <el-button @click="goHome()">返回欢迎页面</el-button>
+
+ 
+</template>
+
+<script>
+export default{
+  name:'login',
+  methods:{
+    goHome(){
+      this.$router.push('/welcom')
+    }
+  }
+}
+
+
+
+
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
--- manger-fe/src/main.js
+++ manger-fe/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css';
+
 console.log(import.meta.env);
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router).use(ElementPlus).mount('#app')
