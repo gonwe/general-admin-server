cd80975b12a9c5305fed0e4bbd4041edf8d252f0
Gonwe <gonwe@qq.com>
2021-07-19 22:50:22 +0800

    feat:log4js封装

---
+++ manger-se/utils/log4j.js
@@ -0,0 +1,60 @@
+/**
+ * 日志存储
+ * @author Gonwe
+ */
+const log4js = require("log4js");
+
+const levels = {
+  trace: log4js.levels.TRACE,
+  debug: log4js.levels.DEBUG,
+  info: log4js.levels.INFO,
+  warn: log4js.levels.WARN,
+  error: log4js.levels.ERROR,
+  fatal: log4js.levels.FATAL,
+};
+
+log4js.configure({
+  appenders: {
+    console: { type: "console" },
+    info: { type: "file", filename: "logs/fileout.log" },
+    error: {
+      type: "dateFile",
+      filename: "logs/errorout.log",
+      pattern: ".yyyy-MM-dd-hh-mm-ss-SSS",
+      alwaysIncludePattern: true, //filename+pattern
+    },
+  },
+  categories: {
+    default: { appenders: ["console"], level: "debug" },
+    info: { appenders: ["console", "info"], level: "info" },
+    error: { appenders: ["console", "error"], level: "error" },
+  },
+});
+
+/**
+ * 日志输出，level为debug
+ * @param {string} context
+ */
+exports.debug = (context) => {
+  let logger = log4js.getLogger();
+  logger.level = levels.debug;
+  logger.debug(context);
+};
+/**
+ * 日志输出，level为info
+ * @param {string} context
+ */
+exports.info = (context) => {
+  let logger = log4js.getLogger("info");
+  logger.level = levels.info;
+  logger.info(context);
+};
+/**
+ * 日志输出，level为error
+ * @param {string} context
+ */
+exports.error = (context) => {
+  let logger = log4js.getLogger("error");
+  logger.level = levels.error;
+  logger.error(context);
+};
