8b0b280fecc19ec256ce5d9026644bcc7c3d6837
Gonwe <gonwe@qq.com>
2021-07-28 23:50:45 +0800

    feat:用户查询接口

--- manger-se/routes/users.js
+++ manger-se/routes/users.js
@@ -20,19 +20,49 @@ router.post("/login", async (ctx) => {
      * 2.{userId：0|1} 1表示传递 -0表示不传递
      * 3.().select('userName userId')字符串内用空格隔开
      */
-    const res = await User.findOne({ userName, userPwd }).select('userId userName userEmail role deptId roleList ');
+    const res = await User.findOne({ userName, userPwd }).select(
+      "userId userName userEmail role deptId roleList "
+    );
     const data = res._doc;
-    const token = jwt.sign({
-      data,
-    },'gonwe',{expiresIn:'1h'})
+    const token = jwt.sign(
+      {
+        data,
+      },
+      "gonwe",
+      { expiresIn: "1h" }
+    );
     data.token = token;
     if (res) {
-      ctx.body = util.success(data,"登录成功！");
+      ctx.body = util.success(data, "登录成功！");
     } else {
       ctx.body = util.fail("账号或者密码不正确", util.CODE.USER_LOGIN_ERROR);
     }
   } catch (error) {
-    ctx.body = util.fail(error.msg,util.CODE.USER_LOGIN_ERROR);
+    ctx.body = util.fail(error.msg, util.CODE.USER_LOGIN_ERROR);
+  }
+});
+
+// 用户列表
+router.get("list", async (ctx) => {
+  const { userId, userName, state } = ctx.request.query;
+  const { page, skipIndex } = util.pager(ctx.request.query);
+  let params = {};
+  if (userId) params.userId = userId;
+  if (userName) params.userId = userName;
+  if (state && state != "0") params.userId = state;
+  try {
+    const query = User.find(params, { _id: 0, userPwd: 0 });
+    const list = await query.skip(skipIndex).limit(page.pageSize);
+    const total = await User.countDocuments(params);
+    ctx.body = util.success({
+      page: {
+        ...page,
+        total,
+      },
+      list,
+    });
+  } catch (error) {
+    ctx.body = util.fail(`查询异常：${error.stack}`);
   }
 });
 
